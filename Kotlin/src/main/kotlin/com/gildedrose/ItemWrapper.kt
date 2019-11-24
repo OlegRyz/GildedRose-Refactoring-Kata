f200040f5b40cfb33c0a1359dfbb28cbf2297940
OlegRyz <aleh.ryzhankou@gmail.com>
2019-11-24 23:18:14 +0100

    Try to bring the same structure to all update methods for further extracting that to ItemWrapper class

--- Kotlin/src/main/kotlin/com/gildedrose/ItemWrapper.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/ItemWrapper.kt
@@ -13,11 +13,10 @@ class AgedBrie(item: Item) : ItemWrapper(item) {
     override fun degrade() {
         if (item.quality < 50) {
             item.quality = item.quality + 1
-        }
-        if (item.sellIn <= 0) {
-            if (item.quality < 50) {
+            if (item.sellIn <= 0) {
                 item.quality = item.quality + 1
             }
+            item.quality = item.quality.coerceAtMost(50)
         }
     }
 }
@@ -43,11 +42,10 @@ class SomeGood(item: Item) : ItemWrapper(item) {
     override fun degrade() {
         if (item.quality > 0) {
             item.quality = item.quality - 1
-        }
-        if (item.sellIn <= 0) {
-            if (item.quality > 0) {
+            if (item.sellIn <= 0) {
                 item.quality = item.quality - 1
             }
+            item.quality = item.quality.coerceAtLeast(0)
         }
     }
 }
