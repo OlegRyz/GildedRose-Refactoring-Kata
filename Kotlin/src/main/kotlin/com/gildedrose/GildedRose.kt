d66ae0e7b207757edab808fe5b667e4bc3822f46
OlegRyz <aleh.ryzhankou@gmail.com>
2019-11-24 21:08:43 +0100

    Replace for with forEach

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,51 +3,51 @@ package com.gildedrose
 class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE) {
-                if (items[i].quality > 0) {
-                    if (items[i].name != SULFURAS) {
-                        items[i].quality = items[i].quality - 1
+        items.forEach {
+            if (it.name != AGED_BRIE && it.name != BACKSTAGE) {
+                if (it.quality > 0) {
+                    if (it.name != SULFURAS) {
+                        it.quality = it.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                if (it.quality < 50) {
+                    it.quality = it.quality + 1
 
-                    if (items[i].name == BACKSTAGE) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                    if (it.name == BACKSTAGE) {
+                        if (it.sellIn < 11) {
+                            if (it.quality < 50) {
+                                it.quality = it.quality + 1
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (it.sellIn < 6) {
+                            if (it.quality < 50) {
+                                it.quality = it.quality + 1
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != SULFURAS) {
-                items[i].sellIn = items[i].sellIn - 1
+            if (it.name != SULFURAS) {
+                it.sellIn = it.sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != AGED_BRIE) {
-                    if (items[i].name != BACKSTAGE) {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != SULFURAS) {
-                                items[i].quality = items[i].quality - 1
+            if (it.sellIn < 0) {
+                if (it.name != AGED_BRIE) {
+                    if (it.name != BACKSTAGE) {
+                        if (it.quality > 0) {
+                            if (it.name != SULFURAS) {
+                                it.quality = it.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        it.quality = it.quality - it.quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (it.quality < 50) {
+                        it.quality = it.quality + 1
                     }
                 }
             }
