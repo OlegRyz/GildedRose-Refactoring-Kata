1766c1dd58e4813a4fa7c1a77c4a3de5479ca89d
OlegRyz <aleh.ryzhankou@gmail.com>
2019-11-24 22:16:11 +0100

    Since we have builder function wrap() that has knowledge about all possible descendants then sealed class for ItemWrapper is the best choice

--- Kotlin/src/main/kotlin/com/gildedrose/ItemWrapper.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/ItemWrapper.kt
@@ -14,7 +14,56 @@ fun wrap(it: Item): ItemWrapper {
     }
 }
 
-abstract class ItemWrapper(val item: Item) {
+class AgedBrie(item: Item) : ItemWrapper(item) {
+    override fun degrade() {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+        }
+        if (item.sellIn <= 0) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1
+            }
+        }
+    }
+}
+
+class BackStage(item: Item) : ItemWrapper(item) {
+    private val qualityRange = 0..50
+
+    override fun degrade() {
+        if (item.quality < 50) {
+            item.quality = item.quality + when (item.sellIn) {
+                in 6..10 -> 2
+                in 0..5 -> 3
+                else -> 1
+            }
+            item.quality = item.quality.coerceIn(qualityRange)
+            if (item.sellIn <= 0) {
+                item.quality = item.quality - item.quality
+            }
+        }
+    }
+}
+
+class SomeGood(item: Item) : ItemWrapper(item) {
+    override fun degrade() {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1
+        }
+        if (item.sellIn <= 0) {
+            if (item.quality > 0) {
+                item.quality = item.quality - 1
+            }
+        }
+    }
+}
+
+class Sulfuras(item: Item) : ItemWrapper(item) {
+    override fun degrade() = Unit
+    override fun moveSellInDate() = Unit
+}
+
+sealed class ItemWrapper(val item: Item) {
     abstract fun degrade()
     open fun moveSellInDate() {
         item.sellIn = item.sellIn - 1
--- Kotlin/src/main/kotlin/com/gildedrose/Sulfuras.kt
+++
@@ -1,6 +0,0 @@
-package com.gildedrose
-
-class Sulfuras(item: Item): ItemWrapper(item) {
-    override fun degrade() = Unit
-    override fun moveSellInDate() = Unit
-}
